d01717434241dcfeb95c8ecce3bae324d775343f
Oscar <rouquette.oscar@gmail.com>
2023-01-05 22:07:28 +0100

    Login requete API

--- src/Login.js
+++ src/Login.js
@@ -6,24 +6,54 @@ import {
     TextInput 
   } from 'react-native';
 import { TouchableOpacity } from "react-native-gesture-handler";
-import { color } from "react-native-reanimated";
-import { Colors } from "react-native/Libraries/NewAppScreen";
 
-export default function({ navigation }) {
-    return(
-        <View style={styles.background}>
-            <Text style={styles.title}>Bienvenue sur (Re)sources Relationnelles !</Text>
-            <TextInput placeholder="Adresse e-mail" placeholderTextColor="black" style={styles.textInput}/>
-            <TextInput placeholder="Mot de passe" placeholderTextColor="black" style={styles.textInput}/>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Menu')}>
-                <Text style={styles.buttonText}>Se connecter</Text>
-            </TouchableOpacity>
-            <Text style={styles.registerText}>Pas encore de compte ?</Text>
-            <TouchableOpacity style={styles.registerButton} onPress={() => navigation.navigate('Menu')}>
-                <Text style={styles.buttonText}>S'inscrire</Text>
-            </TouchableOpacity>
-        </View>
-    )
+export default class Login extends React.Component{
+    render() {
+        return(
+            <View style={styles.background}>
+                <Text style={styles.title}>Bienvenue sur (Re)sources Relationnelles !</Text>
+                <TextInput placeholder="Adresse e-mail" placeholderTextColor="black" style={styles.textInput} onChangeText={mail=>this.setState({mail})}/>
+                <TextInput placeholder="Mot de passe" placeholderTextColor="black" style={styles.textInput} onChangeText={pwd=>this.setState({pwd})}/>
+                <TouchableOpacity style={styles.button} onPress={this.checkLogs}>
+                    <Text style={styles.buttonText}>Se connecter</Text>
+                </TouchableOpacity>
+                <Text style={styles.registerText}>Pas encore de compte ?</Text>
+                <TouchableOpacity style={styles.registerButton} onPress={() => navigator.navigate('Menu')}>
+                    <Text style={styles.buttonText}>S'inscrire</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+    constructor(props)
+    {
+        super(props);
+        navigator = props.navigation;
+    }
+
+    checkLogs = () => {
+        var mail = this.state.mail;
+        var pwd = this.state.pwd;
+        var apiUrl = "http://ezraspberryapis.ddns.net/apis/login.api.php";
+        var formdata = new FormData();
+        var headers = {
+            'Accept':'application/json',
+            'Content-Type':'multipart/form-data',
+        };
+
+        formdata.append("mail",mail);
+        formdata.append("password",pwd);
+
+        fetch(apiUrl,
+        {
+            method:'POST',
+            headers:headers,
+            body:formdata
+        })
+        .then((response)=>response.json())
+        .then((response)=>alert("REPONSE : "+response.message))
+        .catch((error)=>alert("ERREUR : "+error))
+    }
 }
 
 const styles = StyleSheet.create({
@@ -32,12 +62,12 @@ const styles = StyleSheet.create({
         backgroundColor: '#7FF868'
     },
     title: {
-      margin: '10%',
-      marginTop: '15%',
-      fontSize: 25,
-      fontWeight: '800',
-      textAlign: "center",
-      color: 'black'
+    margin: '10%',
+    marginTop: '15%',
+    fontSize: 25,
+    fontWeight: '800',
+    textAlign: "center",
+    color: 'black'
     },
     button: {
         width: '50%',
@@ -79,4 +109,4 @@ const styles = StyleSheet.create({
         height: 25,
         borderRadius: 10
     }
-  });
+});
