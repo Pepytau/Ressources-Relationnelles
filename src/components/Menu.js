f89fbdbae7587c169f2bee9b2324133b478a73fc
Oscar <rouquette.oscar@gmail.com>
2023-01-18 12:10:13 +0100

    Scroll view dans la page menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,25 +3,41 @@ import {
     Text,
     View,
     StyleSheet,
-    Image
+    Image,
+    ScrollView
   } from 'react-native';
 import { useGlobalState } from "../../App"
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function Menu({navigation}){
     const [state, dispatch] = useGlobalState();
+    
+    var payments = [];
+    for(let i = 0; i < 10; i++){
+
+        console.log(payments.length);
+        payments.push(
+            <View key={i} style={styles.ressource}><Text style={styles.ressourceTitle}>Ressource test</Text></View>
+		);
+    }
+
     return(
-        <View style={styles.background}>
-            <Text style={styles.title}>Bonjour {state.name} !</Text>
-            <Text style={styles.secondaryTitle}>Mes ressources :</Text>
-            <View style={styles.bottomTab}>
-                <View style={styles.firstBottomButton}><TouchableOpacity  onPress={() => navigator.navigate('Menu')}><Image style={styles.bottomImages} source={require('../images/home.png')}/><Text style={styles.bottomButtonText}>Menu</Text></TouchableOpacity></View>
-                <View style={styles.bottomButton}><TouchableOpacity onPress={() => navigator.navigate('Seach')}><Image style={styles.bottomImages} source={require('../images/search.png')}/><Text style={styles.bottomButtonText}>Recherche</Text></TouchableOpacity></View>
-                <View style={styles.bottomButton}><TouchableOpacity onPress={() => navigator.navigate('Discover')}><Image style={styles.bottomImages} source={require('../images/discover.png')}/><Text style={styles.bottomButtonText}>Découvrir</Text></TouchableOpacity></View>
-                <View style={styles.bottomButton}><TouchableOpacity onPress={() => navigator.navigate('Favorite')}><Image style={styles.bottomImages} source={require('../images/favorite.png')}/><Text style={styles.bottomButtonText}>Favoris</Text></TouchableOpacity></View>
-                <View style={styles.bottomButton}><TouchableOpacity onPress={() => navigator.navigate('Account')}><Image style={styles.bottomImages} source={require('../images/account.png')}/><Text style={styles.bottomButtonText}>Compte</Text></TouchableOpacity></View>
-            </View>
+    <View style={styles.background}>
+        <Text style={styles.title}>Bonjour {state.name} !</Text>
+        <Text style={styles.secondaryTitle}>Mes ressources :</Text>
+        <View style={styles.scrollViewWrapper}>
+            <ScrollView>
+                {payments}
+            </ScrollView>
         </View>
+        <View style={styles.bottomTab}>
+            <View style={styles.firstBottomButton}><TouchableOpacity  onPress={() => navigator.navigate('Menu')}><Image style={styles.bottomImages} source={require('../images/home.png')}/><Text style={styles.bottomButtonText}>Menu</Text></TouchableOpacity></View>
+            <View style={styles.bottomButton}><TouchableOpacity onPress={() => navigator.navigate('Seach')}><Image style={styles.bottomImages} source={require('../images/search.png')}/><Text style={styles.bottomButtonText}>Recherche</Text></TouchableOpacity></View>
+            <View style={styles.bottomButton}><TouchableOpacity onPress={() => navigator.navigate('Discover')}><Image style={styles.bottomImages} source={require('../images/discover.png')}/><Text style={styles.bottomButtonText}>Découvrir</Text></TouchableOpacity></View>
+            <View style={styles.bottomButton}><TouchableOpacity onPress={() => navigator.navigate('Favorite')}><Image style={styles.bottomImages} source={require('../images/favorite.png')}/><Text style={styles.bottomButtonText}>Favoris</Text></TouchableOpacity></View>
+            <View style={styles.bottomButton}><TouchableOpacity onPress={() => navigator.navigate('Account')}><Image style={styles.bottomImages} source={require('../images/account.png')}/><Text style={styles.bottomButtonText}>Compte</Text></TouchableOpacity></View>
+        </View>
+    </View>
     )
 }
 
@@ -77,5 +93,21 @@ const styles = StyleSheet.create({
         marginBottom: '5%',
         height:30,
         width:30
+    },
+    ressource:{
+        width:'90%',
+        alignSelf: "center",
+        margin: 10,
+        height:130,
+        borderRadius: 10,
+        backgroundColor: "#D7d8d8"
+    },
+    ressourceTitle:{
+        fontSize:25,
+        margin: 10,
+        color: "black"
+    },
+    scrollViewWrapper:{
+        height:'75%'
     }
   });
